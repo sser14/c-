d203456fb8201be3e29ed37a5273833a0832459e
sser14 <sser1481@gmail.com>
2022-09-13 23:56:35 +0900

    first commit

---
+++ Hello.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int num1, num2;
+	printf("두 개의 정수를 입력하세요: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d & %d = %d \n", num1, num2, num1 & num2);
+	printf("%d| %d = %d \n", num1, num2, num1 |num2);
+	printf("%d ^%d = %d \n", num1, num2, num1 ^num2);
+	return 0;
+
+}
